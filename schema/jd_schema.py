6cda65475dd853fb16edfada19762fd5fa76c03a
Swayam <swayamg741@gmail.com>
2026-06-16 10:53:11 +0530

    jd_schema.py

---
+++ schema/jd_schema.py
@@ -0,0 +1,20 @@
+# schema/jd_schema.py
+
+JD_SCHEMA = {
+    "preferred_locations": [],
+    "relocation_preferred": False,
+    "employment_type": [],
+    "work_mode": [],
+    "experience_required": None,
+    "must_have_skills": [], 
+    "preferred_skills": [],
+    "role_focus": [],
+    "education_requirements": [],
+    "company_type_preferences": [], 
+    "negative_signals": [],
+    "notice_period": None,
+    "salary_range": {
+        "min": None,
+        "max": None
+    },
+}
